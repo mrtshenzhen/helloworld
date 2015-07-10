bff18382de0af04a6d6fbe969942314fb6a01c38
marongting <marongting@thinktrader.net>
2015-07-10 10:59:23 +0800

    Signed-off-by: marongting <marongting@thinktrader.net>

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+// helloworld.cpp.cpp : Defines the entry point for the console application.
+//
+
+//#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout<<"hello world!";
+	return 0;
+}
+
